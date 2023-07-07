3e9eac7d87b37b8ccd882a0e952c01398a261990
SarathLUN <lun.sarath@gmail.com>
2023-07-07 13:36:33 +0700

    keep only code completion and auto format on save.

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -16,29 +16,11 @@ local plugins = {
     end
   },
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    ft = "go",
-    opts = function()
-      return require "custom.configs.null-ls"
-    end
-  },
-  {
     "rust-lang/rust.vim",
     ft = "rust",
     init = function ()
       vim.g.rustfmt_autosave = 1
-    end
-  },
-  {
-    "simrat39/rust-tools.nvim",
-    ft = "rust",
-    dependencies = "neovim/nvim-lspconfig",
-    opts = function ()
-      return require "custom.configs.rust-tools"
     end,
-    config = function (_,opts)
-      require('rust-tools').setup(opts)
-    end
   },
 }
 return plugins
